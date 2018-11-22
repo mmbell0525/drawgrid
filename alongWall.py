719c7726a82b05e989e8cdcac1204015072091f2
mmbell0525 <xiux_sun@163.com>
2018-11-22 16:28:48 +0800

    在有比例尺的平面图上画网格线

--- alongWall.py
+++ alongWall.py
@@ -2,34 +2,32 @@ import os
 from numpy import *
 import numpy as np
 import cv2
-# from sys import argv
 
-# a,b,c = argv
 save_path = 'step4/'
 u = 1433
 v = 635
-pts = loadtxt(save_path + "xy_grid.txt",dtype = int)
-print(len(pts))
+pts = loadtxt(save_path + "xy_alongWall.txt",dtype = int)
+print(pts)
 src_pts = pts[:,:2]
 # print(tuple(src_pts[0]))
 # print(src_pts[1])
 dst_pts = pts[:,2:]
 # print(dst_pts)
-# img = cv2.imread(save_path + "image.png")
-img = cv2.imread(save_path + "image_alongWall.jpg")
-for i in range(0,len(pts)):
+img = cv2.imread(save_path +"11fixedImg.jpg")
+for i in range(0,12):
 	# print(src_pts[i][0])
 	src_u = u + (src_pts[i][0]-1) *80
 	src_v = v + (src_pts[i][1]-1) *80
 	dst_u = u + (dst_pts[i][0]-1) *80
 	dst_v = v + (dst_pts[i][1]-1) *80
+	print(src_u,src_v)
 	worldXY = str((src_pts[i][0]-1) *80)+ ' ' + str((src_pts[i][1]-1) *80) + ' ' + str((dst_pts[i][0]-1) *80) + ' '+  str((dst_pts[i][1]-1) *80)
 	print(worldXY)
-	file = open(save_path + "worldXY_grid.txt","a")
+	file = open(save_path +"worldXY_alongWall.txt","a")
 	file.write(worldXY + '\n')
 	file.close()
-	img = cv2.line(img,(int(src_u),int(src_v)),(int(dst_u),int(dst_v)),(255,0,255),2)
+	img = cv2.line(img,(int(src_u),int(src_v)),(int(dst_u),int(dst_v)),(255,0,255),8)
 	# cv2.namedWindow("change",cv2.WINDOW_NORMAL)
 	# cv2.imshow("change", img)
 	# cv2.waitKey(1000)
-cv2.imwrite(save_path + "11image_grid.jpg",img)
+cv2.imwrite(save_path +"image_alongWall.jpg",img)
---
+++ resize_Img.py
@@ -0,0 +1,27 @@
+#1 加载缩放的图片
+import cv2
+img = cv2.imread('step4/fixedImg.jpg',1)
+
+#2 获取图片信息
+imgInfo = img.shape
+print(imgInfo)#打印出图片的宽、高
+# 图片的高、宽
+height = imgInfo[0]
+width = imgInfo[1]
+mode = imgInfo[2]
+
+# 放大 缩小 / 等比例 非等比例
+# 等比例缩小
+# 乘的系数是相同的就是等比例的
+dstHeight = int(height*200/67)
+dstWidth = int(width*200/67)
+
+# 最近临域插值 双线性插值 像素关系重采样 立方插值
+# 3 调用resize方法?是上面哪种方法？
+dst = cv2.resize(img,(dstWidth,dstHeight))
+
+# 4 最终的效果图
+# cv2.imshow('image',dst)
+# cv2.waitKey(0)
+cv2.imwrite('step4/11fixedImg.jpg',dst)
+
